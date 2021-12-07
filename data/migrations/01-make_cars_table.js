a529f559502caf2e6cfe3d2a921523c33459a7ac
jaredhall18 <jaredhall18@gmail.com>
2021-12-07 14:24:03 -0700

    set up migration file and added scrits to migrate

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,27 @@
-exports.up = function (knex) {
-  // DO YOUR MAGIC
+exports.up = async function (knex) {
+ await knex.schema.createTable('fruits', table => {
+  table.increments('car_id')
+
+  table.text('car_vin')
+    .unique()
+    .notNullable()
+
+  table.text('car_make')
+    .notNullable()
+
+  table.text('car_model')
+    .notNullable()
+
+  table.decimal('mileage')
+    .notNullable()
+
+    table.text('car_title')
+
+    table.text('car_transmission')
+ 
+ })
 };
 
-exports.down = function (knex) {
-  // DO YOUR MAGIC
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists('cars')
 };
